44422443defa1567842af2554a4cbe6af1427151
Ariel Nunez <ingenieroariel@gmail.com>
2018-11-26 12:33:23 -0500

    Working csv example

--- index.go
+++ index.go
@@ -6,15 +6,29 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"os"
+	"strings"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	fileName := "data.csv"
-	f, _ := os.Open(fileName)
-	reader := csv.NewReader(f)
-	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+func data() string {
+	in := `first_name;last_name;username
+"Rob";"Pike";rob
+# lines beginning with a # character are ignored
+Ken;Thompson;ken
+"Robert";"Griesemer";"gri"
+`
+	return in
+}
 
+func Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Reading the first line of a csv file</h1>")
+
+	reader := csv.NewReader(strings.NewReader(data()))
+	reader.Comma = ';'
+	reader.Comment = '#'
+
+	if reader != nil {
+		fmt.Fprintf(w, "Got  file")
+	}
 	for {
 		record, err := reader.Read()
 		if err == io.EOF {
@@ -22,8 +36,10 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		}
 		if err != nil {
 			log.Fatal(err)
+		} else {
+
+			fmt.Fprintf(w, "%s\n", record)
 		}
 
-		fmt.Fprintf(w, "%s\n", record)
 	}
 }
